c2bfe296336d548e929ccf6915dac8c7d60d9184
CryoCardiogram <menalewa@live.com>
2021-10-27 18:12:17 +0200

    dl and build script

---
+++ Bipartite-Matching_CORA/get_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo 'downloading preprocessed CORA dataset...'
+wget -cq https://bryanwilder.github.io/files/data_decisions_benchmarks.zip && unzip -qqn data_decisions_benchmarks.zip 
+mv benchmarks_release/make_cora_dataset.py make_cora_dataset.py
+echo 'done'
+echo 'cleaning...'
+# delete zip
+rm data_decisions_benchmarks.zip 
+
+# delete non-relevant data
+rm benchmarks_release/budget_allocation_data.pickle benchmarks_release/diverse_recommendation_data.pickle benchmarks_release/readme.txt
+
+echo 'done'
