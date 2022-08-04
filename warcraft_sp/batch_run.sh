36553eaa668a679dd04c2f33ad4245632fc236f4
jayman91 <jayantam2017@email.iimcal.ac.in>
2022-08-04 15:19:49 -0400

    unnecessary packages in requirements removed

--- warcraft_sp/batch_run.sh
+++ warcraft_sp/batch_run.sh
@@ -8,5 +8,5 @@ d=${4}
 
 source ~/.bashrc
 source warcraft_venv/bin/activate
-python Test${a}.py --img_size ${b} --output_tag ${c} --index ${d}   > ./log/runs_${d}.log
+python Test${a}.py --img_size ${b} --output_tag ${c} --index ${d}   > ./log/4aug_runs_${d}.log
 exit 0
