3b4801f4a04415a818267791d0831ad88f27d21a
JayMan91 <jayantam2017@email.iimcal.ac.in>
2022-08-04 17:54:07 -0400

    img size 12 run on condor

--- warcraft_sp/batch_run.sh
+++ warcraft_sp/batch_run.sh
@@ -5,9 +5,9 @@ imgsz=${2}
 lr=${3}
 tag= ${4}
 id=${5}
-
+echo ${tag}
 
 source ~/.bashrc
 source warcraft_venv/bin/activate
-python Test${modelname}.py --img_size ${imgsz} --lr ${lr} --output_tag ${tag} --index ${id}   > ./log/aug4evning_${id}.log
+python Test${modelname}.py --img_size ${imgsz} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/twostagerun_${id}.log
 exit 0
