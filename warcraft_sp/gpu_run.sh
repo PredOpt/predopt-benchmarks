c00786d2fa1d95c033e9d42b041d7dd9e77a8968
JayMan91 <jayantam2017@email.iimcal.ac.in>
2022-08-03 23:31:15 -0400

    tryingspo with gpu

---
+++ warcraft_sp/gpu_run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+a=${1}
+b=${2}
+c=${3}
+d=${4}
+
+
+source ~/.bashrc
+source warcraft_venv/bin/activate
+python Test${a}.py --img_size ${b} --output_tag ${c} --index ${d}   > ./log/gpuruns_${d}.log
+exit 0
