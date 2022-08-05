832520f3e869abc6535aa482adc91264703fd22e
JayMan91 <jayantam2017@email.iimcal.ac.in>
2022-08-04 21:41:50 -0400

    IMPLE codes

--- warcraft_sp/imle_run.sh
+++ warcraft_sp/imle_run.sh
@@ -13,5 +13,5 @@ id=${10}
 
 source ~/.bashrc
 source warcraft_venv/bin/activate
-python Test${modelname}.py --img_size ${imgsz} --input_noise_temp ${inptemp} --target_noise_temp ${trgttemp} --num_samples ${nsamp} --num_iter ${niter} --k {k} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/imlerun_${id}.log
+python Test${modelname}.py --img_size ${imgsz} --input_noise_temp ${inptemp} --target_noise_temp ${trgttemp} --num_samples ${nsamp} --num_iter ${niter} --k ${k} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/imlerun_${id}.log
 exit 0
