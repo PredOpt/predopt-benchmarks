42e8c5a74833a47e70de3392ff421ea5ccb31594
JayMan91 <jayantam2017@email.iimcal.ac.in>
2022-09-01 21:28:00 -0400

    dpo warcraft

--- warcraft_sp/imle_run.sh
+++ warcraft_sp/imle_run.sh
@@ -7,11 +7,12 @@ trgttemp=${4}
 nsamp=${5}
 niter=${6}
 k=${7}
-lr=${8}
-tag= ${9}
-id=${10}
+seed=${8}
+lr=${9}
+tag= ${10}
+id=${11}
 
 source ~/.bashrc
 source warcraft_venv/bin/activate
-python Test${modelname}.py --img_size ${imgsz} --input_noise_temp ${inptemp} --target_noise_temp ${trgttemp} --num_samples ${nsamp} --num_iter ${niter} --k ${k} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/imlerun_${id}.log
+python Test${modelname}.py --img_size ${imgsz} --input_noise_temp ${inptemp} --target_noise_temp ${trgttemp} --num_samples ${nsamp} --num_iter ${niter} --k ${k} --seed ${seed} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/imlerun_${id}.log
 exit 0
