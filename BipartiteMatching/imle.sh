59c8106595aaec8ae3cb2d46a6f66b16927592d8
JayMan91 <jayantam2017@email.iimcal.ac.in>
2022-09-03 19:26:39 -0400

    experiment with more layers

--- BipartiteMatching/imle.sh
+++ BipartiteMatching/imle.sh
@@ -5,13 +5,14 @@ instance=${2}
 inptemp=${3}
 targettemp=${4}
 k=${5}
-niter=${6}
-lr=${7}
-tag=${8}
-id=${9}
+nsamp=${6}
+niter=${7}
+lr=${8}
+tag=${9}
+id=${10}
 echo ${tag}
 
 source ~/.bashrc
 source ../warcraft_sp/warcraft_venv/bin/activate
-python test_matching_${modelname}.py --instance ${instance} --input_noise_temp "${inptemp}" --target_noise_temp "${targettemp}" --k ${k}  --num_iter ${niter} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/IMLE_${id}.log
+python test_matching_${modelname}.py --instance ${instance} --num_samples ${nsamp} --input_noise_temp "${inptemp}" --target_noise_temp "${targettemp}" --k ${k}  --num_iter ${niter} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/IMLE_${id}.log
 exit 0
