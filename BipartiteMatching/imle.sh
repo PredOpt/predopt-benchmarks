4768bca090d530328d30b57e0bbecee20c857ebe
JayMan91 <jayantam2017@email.iimcal.ac.in>
2022-09-21 13:48:31 -0400

    one script

--- BipartiteMatching/imle.sh
+++ BipartiteMatching/imle.sh
@@ -2,18 +2,17 @@
 
 modelname=${1}
 instance=${2}
-inptemp=${3}
-targettemp=${4}
-beta=${5}
-k=${6}
-nsamp=${7}
-niter=${8}
-lr=${9}
-tag=${10}
-id=${11}
+temp=${3}
+beta=${4}
+k=${5}
+nsamp=${6}
+niter=${7}
+lr=${8}
+tag=${9}
+id=${10}
 echo ${tag}
 
 source ~/.bashrc
 source ../warcraft_sp/warcraft_venv/bin/activate
-python test_matching_${modelname}.py --instance ${instance} --num_samples ${nsamp} --beta "${beta}" --input_noise_temp "${inptemp}" --target_noise_temp "${targettemp}" --k ${k}  --num_iter ${niter} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/IMLE_${id}.log
+python test_matching.py --model ${modelname} --instance ${instance} --num_samples ${nsamp} --beta "${beta}" --itemperature "${temp}" --target_noise_temp "${targettemp}" --k ${k}  --num_iter ${niter} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/IMLE_${id}.log
 exit 0
