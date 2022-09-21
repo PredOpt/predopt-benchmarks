96e0efdc252ceac45129aee8ae5ab613404e118d
JayMan91 <jayantam2017@email.iimcal.ac.in>
2022-09-21 13:23:45 -0400

    one script

--- BipartiteMatching/baseline.sh
+++ BipartiteMatching/baseline.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-modelname=${1}
+modelname=${1}	
 instance=${2}
 lr=${3}
 tag=${4}
@@ -9,5 +9,5 @@ echo ${tag}
 
 source ~/.bashrc
 source ../warcraft_sp/warcraft_venv/bin/activate
-python test_matching_${modelname}.py --instance ${instance} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/baseline_${id}.log
+python test_matching.py --model ${modelname} --instance ${instance} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/baseline_${id}.log
 exit 0
