25d5e0bc501826c2112945fe38543c78a0d5da6d
JayMan91 <jayantam2017@email.iimcal.ac.in>
2022-09-21 13:36:35 -0400

    one script

--- BipartiteMatching/blackbox.sh
+++ BipartiteMatching/blackbox.sh
@@ -10,5 +10,5 @@ echo ${tag}
 
 source ~/.bashrc
 source ../warcraft_sp/warcraft_venv/bin/activate
-python test_matching_${modelname}.py --instance ${instance} --lambda_val "${lamda}"  --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/baseline_${id}.log
+python test_matching.py --model ${modelname} --instance ${instance} --lambda_val "${lamda}"  --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/baseline_${id}.log
 exit 0
