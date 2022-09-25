a9ef4d464cb1e0680e4c80e720f760c9abb26f27
JayMan91 <jayantam2017@email.iimcal.ac.in>
2022-09-25 04:30:25 +0200

    baseline knpasack

--- Knapsack/testknapsack.py
+++ Knapsack/testknapsack.py
@@ -39,6 +39,8 @@ parser.add_argument("--lr", type=float, help="learning rate", default= 1e-3, req
 parser.add_argument("--batch_size", type=int, help="batch size", default= 128, required=False)
 parser.add_argument("--max_epochs", type=int, help="maximum number of epochs", default= 50, required=False)
 
+parser.add_argument("--output_tag", type=str, help="tag", default= 50, required=False)
+parser.add_argument("--index", type=int, help="index", default= 50, required=False)
 args = parser.parse_args()
 
 class _Sentinel:
