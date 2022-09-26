1830a704379a507f3edbc046b07ef61b7100bfdf
JayMan91 <jayantam2017@email.iimcal.ac.in>
2022-09-26 17:03:55 +0200

    warcraft bce'

--- warcraft_sp/baseline.sh
+++ warcraft_sp/baseline.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 modelname=${1}
-imgsz=${2}
-seed=${3}
-lr=${4}
-tag=${5}
-id=${6}
+loss=${2}
+imgsz=${3}
+seed=${4}
+lr=${5}
+tag=${6}
+id=${7}
 echo ${tag}
 
 source ~/.bashrc
 source warcraft_venv/bin/activate
-python Test${modelname}.py --img_size ${imgsz} --seed ${seed} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/baseline_${id}.log
+python TestWarcraft.py --model ${modelname} --loss ${loss} --img_size ${imgsz} --seed ${seed} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/baseline_${id}.log
 exit 0
