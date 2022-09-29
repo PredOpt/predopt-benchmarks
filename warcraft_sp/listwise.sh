6d6558251d6ff23c5df102c61d469863500bb701
JayMan91 <jayantam2017@email.iimcal.ac.in>
2022-09-29 11:48:21 +0200

    ranking

--- warcraft_sp/listwise.sh
+++ warcraft_sp/listwise.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 modelname=${1}
-imgsz=${2}
-tau=${3}
-growth=${4}
-seed=${5}
-lr=${6}
-tag=${7}
-id=${8}
+loss=${2}
+imgsz=${3}
+tau=${4}
+growth=${5}
+seed=${6}
+lr=${7}
+tag=${8}
+id=${9}
 echo ${tag}
 
 source ~/.bashrc
 source warcraft_venv/bin/activate
-python Test${modelname}.py --img_size ${imgsz} --growth ${growth} --lr "${lr}" --tau "${tau}" --seed ${seed} --output_tag "${tag}" --index ${id}   > ./log/Listwise_${id}.log
+python TestWarcraft.py --model ${modelname} --loss ${loss} --img_size ${imgsz} --growth ${growth} --lr "${lr}" --tau "${tau}" --seed ${seed} --output_tag "${tag}" --index ${id}   > ./log/Listwise_${id}.log
 exit 0
--- warcraft_sp/ranking_run.sh
+++ warcraft_sp/ranking_run.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 modelname=${1}
-imgsz=${2}
-growth=${3}
-seed=${4}
-lr=${5}
-tag=${6}
-id=${7}
+loss=${2}
+imgsz=${3}
+growth=${4}
+seed=${5}
+lr=${6}
+tag=${7}
+id=${8}
 echo ${tag}
 
 source ~/.bashrc
 source warcraft_venv/bin/activate
-python Test${modelname}.py --img_size ${imgsz} --growth ${growth} --seed ${seed} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/Rankingrun1_${id}.log
+python TestWarcraft.py --model ${modelname} --loss ${loss} --img_size ${imgsz} --growth ${growth} --seed ${seed} --lr "${lr}" --output_tag "${tag}" --index ${id}   > ./log/Rankingrun1_${id}.log
 exit 0
